4cfcec2b049ce0fbc6ee6ca0fa6a1300de3c9b42
Lauro Brant <brantlauro@gmail.com>
2021-06-17 20:54:23 -0300

    Update 7-odds_and_evens.py

--- 7-odds_and_evens.py
+++ 7-odds_and_evens.py
@@ -21,5 +21,36 @@ print('''
 ''')
 
 din("Welcome to Lauro's Odds and Evens! \n"
-    "You choose between Odd and Even, Then you enter a number, the pc randomize another... \n"
-    "This numbers are sum, and if you was chosen thw right parity you wil")
+    "You choose between Odd and Even, Then you enter a number, the pc randomize another (1 to 10)... \n"
+    "This numbers are sum, and if you was chosen the right parity you win!")
+lin(30, "-=-")
+
+n = c = 0
+
+while True:
+    choose = ''
+    pc = rd(1, 10)
+    n = int(input('Type a number between 1 and 10: '))
+    if 1 <= n <= 10:
+        choose = input('Even or Odd? [E/O] ').strip().upper()[0]
+        if choose == 'E':
+            if (pc + n) % 2 == 0:
+                din(f'Pc choose {pc}. {pc + n} is Even! You won congrats!')
+                c += 1
+            else:
+                din(f'Pc choose {pc}. {pc + n} is Odd! You lost, sorry!')
+                break
+        elif choose == 'O':
+            if (pc + n) % 2 != 0:
+                din(f'Pc choose {pc}. {pc + n} is Odd! You won congrats!')
+                c += 1
+            else:
+                din(f'Pc choose {pc}. {pc + n} is Even! You lost, sorry!')
+                break
+        else:
+            din('[ERROR] Try Again!')
+    else:
+        din('Type a number between 1 and 10!')
+lin(30, "-=-")
+din(f'You won {c} times!')
+
